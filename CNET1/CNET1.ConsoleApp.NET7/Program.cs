65d00b8aa4140f7e511df208e2199fd7ecb58dc8
nterranova <58537070+nterranova@users.noreply.github.com>
2023-12-27 09:51:33 -0600

    running age tester

--- CNET1/CNET1.ConsoleApp.NET7/Program.cs
+++ CNET1/CNET1.ConsoleApp.NET7/Program.cs
@@ -9,6 +9,8 @@ namespace CNET1.ConsoleApp.NET7
             Console.WriteLine("Hello, World!");
             var i = new AgeTester();
             i.LaunchProcess();
+
+            var myList = new List<T>();
         }
     }
 }
