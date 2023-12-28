06f264a58d9884de52aa572d9be89c1ac5e17410
nterranova <58537070+nterranova@users.noreply.github.com>
2023-12-28 14:44:04 -0600

    added generic swapper

---
+++ CNET2/CNET2.ConsoleApp.NET7.Library/Helpers/GenericSwapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNET2.ConsoleApp.NET7.Library.Helpers
+{
+    public static class GenericSwapper
+    {
+        public static void SwapObjects<T>(ref T object1, ref T object2)
+        {
+            T tempObject = object1;
+            object1 = object2;
+            object2 = tempObject;
+        }
+    }
+}
---
+++ CNET2/CNET2.ConsoleApp.NET7.Library/Interfaces/IMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNET2.ConsoleApp.NET7.Library.Interfaces
+{
+    public interface IMapper<S, T>
+    {
+        T Map(S source);
+    }
+}
